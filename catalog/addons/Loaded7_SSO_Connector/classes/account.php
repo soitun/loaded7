baace772b341db00dd8d9af84195951e034333c6
gulsarrays <gulmohar@loadedcommerce.com>
2013-08-27 11:26:12 +0530

    D1221 -  WORDPRESS SSO -  LOADED7 SSO Connector

---
+++ catalog/addons/Loaded7_SSO_Connector/classes/account.php
@@ -0,0 +1,126 @@
+<?php
+/*
+  $Id: account.php v1.0 2013-01-01 datazen $
+
+  LoadedCommerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     LoadedCommerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+
+/**
+ * The lC_Account class manages customer accounts
+ */
+
+  class lC_Account_sso_connector extends lC_Template {
+  
+ /**
+ * Checks if a customer account record exists with the provided external_id
+ *
+ * @param string $email_address The e-mail address to check for
+ * @access public
+ * @return boolean
+ */
+
+    public static function checkExternalID($external_id) {
+      global $lC_Database;
+
+      $Qcheck = $lC_Database->query('select customers_id from :table_customers where customers_external_id = :customers_external_id limit 1');
+      $Qcheck->bindTable(':table_customers', TABLE_CUSTOMERS);
+      $Qcheck->bindValue(':customers_external_id', $external_id);
+      $Qcheck->execute();
+
+      return ( $Qcheck->numberOfRows() === 1 );
+    }
+
+ /**
+ * Checks if a customer account record exists with the provided e-mail address and external_id
+ *
+ * @param string $email_address The e-mail address to check for
+ * @access public
+ * @return boolean
+ */
+
+    public static function checkEmailforExternalID($email_address,$external_id) {
+      global $lC_Database;
+
+      $Qcheck = $lC_Database->query('select customers_id from :table_customers where customers_email_address = :customers_email_address and customers_external_id = :customers_external_id limit 1');
+      $Qcheck->bindTable(':table_customers', TABLE_CUSTOMERS);
+      $Qcheck->bindValue(':customers_email_address', $email_address);
+      $Qcheck->bindValue(':customers_external_id', $external_id);
+      $Qcheck->execute();
+
+      return ( $Qcheck->numberOfRows() === 1 );
+    }
+
+      /* Private methods */   
+    function _processSSO() {
+      global $lC_Database, $lC_Session, $lC_Language, $lC_ShoppingCart, $lC_MessageStack, $lC_Customer, $lC_NavigationHistory, $lC_Vqmod;
+      
+      require($lC_Vqmod->modCheck('includes/classes/account.php'));
+      
+      $redirect = false;
+      $data = array();
+
+
+      if (lC_Account::checkExternalID($_GET['external_id'])) {
+        
+        if (lC_Account::checkEmailforExternalID($_GET['email'],$_GET['external_id'])) {        
+          $redirect = true;
+        } else if(lC_Account::checkDuplicateEntry($_GET['email']) === false) {        
+          // update email for existing customers_external_id
+          $Qupdate = $lC_Database->query('update :table_customers set customers_email_address = :customers_email_address where customers_external_id = :customers_external_id');
+          $Qupdate->bindTable(':table_customers', TABLE_CUSTOMERS);
+          $Qupdate->bindValue(':customers_email_address', $_GET['email']);
+          $Qupdate->bindInt(':customers_external_id', $_GET['external_id']);
+          $Qupdate->execute();
+          $redirect = true;
+        }
+      } else if (lC_Account::checkEntry($_GET['email'])) {     
+        // update customers_external_id for existing email_address
+          $Qupdate = $lC_Database->query('update :table_customers set customers_external_id = :customers_external_id where customers_email_address = :customers_email_address');
+          $Qupdate->bindTable(':table_customers', TABLE_CUSTOMERS);
+          $Qupdate->bindValue(':customers_email_address', $_GET['email']);
+          $Qupdate->bindInt(':customers_external_id', $_GET['external_id']);
+          $Qupdate->execute();
+          $redirect = true;
+      } else {
+        $error = false;
+        $customers_name = explode('-',$_GET['name'],2);
+        $data['external_id'] = $_GET['external_id'];
+        $data['password'] = mktime();
+
+        if (isset($customers_name[0]) && (strlen(trim($customers_name[0])) >= ACCOUNT_FIRST_NAME)) {
+          $data['firstname'] = $customers_name[0];
+        } else {
+          $error = true;
+        }
+
+        if (isset($customers_name[1]) && (strlen(trim($customers_name[1])) >= ACCOUNT_LAST_NAME)) {
+          $data['lastname'] = $customers_name[1];
+        } else {
+          $error = true;
+        }
+
+        if (lC_Account::checkDuplicateEntry($_GET['email']) === false) {
+          $data['email_address'] = $_GET['email'];
+        } else {
+          $error = true;
+        } 
+
+        if ( $error == false && lC_Account::createEntry($data)) {
+          $redirect == true;
+        }                  
+      }
+      if($redirect == true) {
+        lc_redirect($_GET['redirect']);
+      }
+      
+    }
+
+  }
+?>
---
+++ catalog/addons/Loaded7_SSO_Connector/controller.php
@@ -0,0 +1,94 @@
+<?php
+/*
+  $Id: controller.php v1.0 2013-04-20 datazen $
+
+  Loaded Commerce, Innovative eCommerce Solutions
+  http://www.loadedcommerce.com
+
+  Copyright (c) 2013 Loaded Commerce, LLC
+
+  @author     Loaded Commerce Team
+  @copyright  (c) 2013 LoadedCommerce Team
+  @license    http://loadedcommerce.com/license.html
+*/
+class Loaded7_SSO_Connector extends lC_Addon { // your addon must extend lC_Addon
+  /*
+  * Class constructor
+  */
+  public function Loaded7_SSO_Connector() {    
+    global $lC_Language;    
+   /**
+    * The addon type (category)
+    * valid types; payment, shipping, themes, checkout, catalog, admin, reports, connectors, other 
+    */    
+    $this->_type = 'connectors';
+   /**
+    * The addon class name
+    */    
+    $this->_code = 'Loaded7_SSO_Connector';       
+   /**
+    * The addon title used in the addons store listing
+    */     
+    $this->_title = $lC_Language->get('addon_loaded7_sso_connector_title');
+   /**
+    * The addon description used in the addons store listing
+    */     
+    $this->_description = $lC_Language->get('addon_loaded7_sso_connector_description');
+   /**
+    * The developers name
+    */    
+    $this->_author = 'Loaded Commerce, LLC';
+   /**
+    * The developers web address
+    */    
+    $this->_authorWWW = 'http://www.loadedcommerce.com';    
+   /**
+    * The addon version
+    */     
+    $this->_version = '1.0.0'; 
+   /**
+    * The addon image used in the addons store listing
+    */     
+    $this->_thumbnail = lc_image(DIR_WS_CATALOG . 'addons/' . $this->_code . '/images/logo.png');
+   /**
+    * The addon enable/disable switch
+    */    
+    $this->_enabled = (defined('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS') && @constant('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS') == '1') ? true : false;
+  }
+ /**
+  * Checks to see if the addon has been installed
+  *
+  * @access public
+  * @return boolean
+  */
+  public function isInstalled() {
+    return (bool)defined('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS');
+  }
+ /**
+  * Install the addon
+  *
+  * @access public
+  * @return void
+  */
+  public function install() {
+    global $lC_Database;
+
+    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Enable AddOn', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_STATUS', '-1', 'Do you want to enable this addon?', '6', '0', 'lc_cfg_use_get_boolean_value', 'lc_cfg_set_boolean_value(array(1, -1))', now())");
+
+    $lC_Database->simpleQuery("ALTER TABLE " . TABLE_CUSTOMERS . " ADD `customers_external_id` INT( 11 ) NULL , ADD UNIQUE (`customers_external_id`)");
+  }
+ /**
+  * Return the configuration parameter keys an an array
+  *
+  * @access public
+  * @return array
+  */
+  public function getKeys() {
+    if (!isset($this->_keys)) {
+      $this->_keys = array('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS');
+    }
+
+    return $this->_keys;
+  }  
+}
+?>
