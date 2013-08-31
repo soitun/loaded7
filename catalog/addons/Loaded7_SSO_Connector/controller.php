f11644c63f904d67e0404b26cc63f2ae1ff61cfa
gulsarrays <gulmohar@loadedcommerce.com>
2013-08-31 13:41:45 +0530

    D1221 -  WORDPRESS SSO -  LOADED7 SSO Connector

--- catalog/addons/Loaded7_SSO_Connector/controller.php
+++ catalog/addons/Loaded7_SSO_Connector/controller.php
@@ -76,13 +76,8 @@ class Loaded7_SSO_Connector extends lC_Addon { // your addon must extend lC_Addo
     $encrypted_key = hash('sha256', mktime());
 
     $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Enable AddOn', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_STATUS', '-1', 'Do you want to enable this addon?', '6', '0', 'lc_cfg_use_get_boolean_value', 'lc_cfg_set_boolean_value(array(1, -1))', now())");
-    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Authentication Token', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_AUTHENTICATION_TOKEN', '".$encrypted_key."', '', '6', '0', 'lc_cfg_set_readonly_input_field', now())");
-
-
- 
-    
-    
-    
+    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Wordpress URL', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_WORDPRESS_URL', '', '\"http://www.mywordepress.com\" for top level domain, \"http://wordepress.mystore.com\" for subdomains, \"http://www.mystore.com/wordepress/\" for a folder on a top level domain', '6', '0', now())");
+    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Authentication Token', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_AUTHENTICATION_TOKEN', '".$encrypted_key."', '', '6', '0', 'lc_cfg_set_readonly_input_field', now())");    
 
     $lC_Database->simpleQuery("ALTER TABLE " . TABLE_CUSTOMERS . " ADD `customers_external_id` INT( 11 ) NULL , ADD UNIQUE (`customers_external_id`)");
   }
@@ -94,7 +89,7 @@ class Loaded7_SSO_Connector extends lC_Addon { // your addon must extend lC_Addo
   */
   public function getKeys() {
     if (!isset($this->_keys)) {
-      $this->_keys = array('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS','ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_AUTHENTICATION_TOKEN');
+      $this->_keys = array('ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_STATUS', 'ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_WORDPRESS_URL', 'ADDONS_CONNECTORS_' . strtoupper($this->_code) . '_AUTHENTICATION_TOKEN');
     }
 
     return $this->_keys;
