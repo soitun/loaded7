22429334b6625a8274acdc986b76d5f8f718c2e1
Gerald Bullard Jr <gerald@thereactorworks.com>
2013-09-13 15:56:40 -0400

    more small formatting clean up

--- catalog/addons/Loaded7_SSO_Connector/controller.php
+++ catalog/addons/Loaded7_SSO_Connector/controller.php
@@ -11,7 +11,6 @@
   @copyright  (c) 2013 LoadedCommerce Team
   @license    http://loadedcommerce.com/license.html
 */
-//require($lC_Vqmod->modCheck('../includes/classes/utility.php'));
 class Loaded7_SSO_Connector extends lC_Addon { // your addon must extend lC_Addon
   /*
   * Class constructor
@@ -78,10 +77,10 @@ class Loaded7_SSO_Connector extends lC_Addon { // your addon must extend lC_Addo
 
     $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Enable AddOn', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_STATUS', '-1', 'Do you want to enable this addon?', '6', '0', 'lc_cfg_use_get_boolean_value', 'lc_cfg_set_boolean_value(array(1, -1))', now())");
     $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Remote Login URL', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_REMOTE_LOGIN_URL', '', 'Put Remote Login URL given in Wordpress Loaded7 SSO Options', '6', '0', 'lc_cfg_set_textarea_field', now())");
-    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Remote Logout URL', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_REMOTE_LOGOUT_URL', '', 'Put Remote Logout URL given in Wordpress Loaded7 SSO Options', '6', '0', 'lc_cfg_set_textarea_field()', now())");
+    $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Remote Logout URL', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_REMOTE_LOGOUT_URL', '', 'Put Remote Logout URL given in Wordpress Loaded7 SSO Options', '6', '0', 'lc_cfg_set_textarea_field', now())");
     $lC_Database->simpleQuery("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Authentication Token', 'ADDONS_CONNECTORS_" . strtoupper($this->_code) . "_AUTHENTICATION_TOKEN', '".$encrypted_key."', '', '6', '0', 'lc_cfg_set_readonly_input_field', now())");    
 
-    $lC_Database->simpleQuery("ALTER TABLE " . TABLE_CUSTOMERS . " ADD `external_id` INT( 11 ) NULL , ADD UNIQUE (`external_id`)");
+    $lC_Database->simpleQuery("alter table " . TABLE_CUSTOMERS . " add external_id int( 11 ) null, add unique (external_id)");
   }
  /**
   * Return the configuration parameter keys an an array
